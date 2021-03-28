4c7f94e8fe66e7480a178c36a40c5f82086509af
Obinna Ezekwe <ozekwe@gmail.com>
2021-03-27 20:10:34 -0500

    Added link to application and change name in title and header

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # This is a e-commerce application recoded to use Redux instead of Context API.
 
-## Link to application: 
+## Link to application: https://shop2shop.herokuapp.com/
 
 ## Screenshot: 
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -44,7 +44,7 @@ function Nav() {
       <h1>
         <Link to="/">
           <span role="img" aria-label="shopping bag">🛍️</span>
-          Shop-Shop
+          Shop2Shop
         </Link>
       </h1>
 
